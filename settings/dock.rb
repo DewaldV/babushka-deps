6ea3c858a114c8ef3a2ac708697e37b6a09e7a98
DewaldV <dewald.viljoen@gmail.com>
2014-09-25 17:16:21 +0200

    Add check for existence to dockutil template. Consider dependency met if app is not present.

--- settings/dock.rb
+++ settings/dock.rb
@@ -14,6 +14,25 @@ dep 'dock-magnification.defaults' do
   value '0'
 end
 
+dep 'dock-tilesize.defaults' do
+  key 'tilesize'
+  domain 'com.apple.dock'
+  type 'int'
+  value '48'
+end
+
+dep 'dock-remove-all-icons' do
+  met? { shell('dockutil --list') == '' }
+  meet { shell('dockutil --remove all') }
+end
+
+dep 'dock-size-immutable.defaults' do
+  key 'size-immutable'
+  domain 'com.apple.dock'
+  type 'int'
+  value '1'
+end
+
 dep 'documents.dockicon' do
   label 'Documents'
   path '~/Documents' 
@@ -30,14 +49,64 @@ dep 'home.dockicon' do
 end
 
 dep 'dock-default-icons' do
+  requires 'dock-remove-all-icons'
   requires 'home.dockicon'
   requires 'documents.dockicon'
   requires 'downloads.dockicon'
 end
 
+dep 'safari.dockicon' do
+  label 'Safari'
+  path '/Applications/Safari.app'
+end
+
+dep 'itunes.dockicon' do
+  label 'iTunes'
+  path '/Applications/iTunes.app'
+end
+
+dep 'firefox.dockicon' do
+  label 'FireFox'
+  path '/Applications/Firefox.app'
+end
+
+dep 'opera.dockicon' do
+  label 'Opera'
+  path '/Applications/Opera.app'
+end
+
+dep 'chrome.dockicon' do
+  label 'Chrome'
+  path '/Applications/Google\ Chrome.app'
+end
+
+dep 'steam.dockicon' do
+  label 'Steam'
+  path '/Applications/Steam.app'
+end
+
+dep 'iterm2.dockicon' do
+  label 'iTerm'
+  path '~/Applications/iTerm.app'
+end
+
+dep 'dock-default-apps' do
+  requires 'dock-default-icons'
+  requires 'opera.dockicon'
+  requires 'chrome.dockicon'
+  requires 'firefox.dockicon'
+  requires 'safari.dockicon'
+  requires 'itunes.dockicon'
+  requires 'steam.dockicon'
+  requires 'iterm2.dockicon'
+end
+
 dep 'osx-settings-dock' do
   requires 'dock-autohide.defaults'
   requires 'dock-magnification.defaults'
+  requires 'dock-tilesize.defaults'
+  requires 'dock-size-immutable.defaults'
   requires 'dock-default-icons'
+  requires 'dock-default-apps'
   after { 'killall -HUP Dock' }
 end
--- templates/osx-dockutil.rb
+++ templates/osx-dockutil.rb
@@ -5,7 +5,7 @@ meta :dockicon do
   accepts_value_for :path
 
   template {
-    met? { shell?("dockutil --find #{label}") }
+    met? { !shell?("[ -d #{path} ]") || shell?("dockutil --find #{label}") }
     meet { shell("dockutil --add #{path} --label #{label}") }
   }
 end
