2fce159f0481069253ce2c4de10b80f940e129fe
DewaldV <dewald.viljoen@gmail.com>
2014-09-25 12:05:54 +0200

    Add net packages.

--- apps/net.rb
+++ apps/net.rb
@@ -13,3 +13,22 @@ dep 'opera.cask'
 dep 'skype.cask'
 
 dep 'teamspeak-client.cask'
+
+dep 'osx-net-alt-browsers' do
+  requires 'firefox.cask'
+  requires 'google-chrome.cask'
+  requires 'opera.cask'
+end
+
+dep 'osx-net-im' do
+  requires 'irssi.bin'
+end
+
+dep 'osx-net-voip' do
+  requires 'skype.cask'
+  requires 'teamspeak-client.cask'
+end
+
+dep 'osx-net-servers' do
+  requires 'nginx.bin'
+end
