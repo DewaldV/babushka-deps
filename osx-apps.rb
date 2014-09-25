c7b349a7200d0294fe7bbe50dbc5c774c820e17c
DewaldV <dewald.viljoen@gmail.com>
2014-09-25 12:41:56 +0200

    Add base.

--- osx-apps.rb
+++
@@ -1,9 +0,0 @@
-# osx-apps.rb
-
-dep "all-osx-apps" do
-  requires "dropbox.cask"
-  requires "iterm.cask"
-  requires "macvim.cask"
-  requires "rescuetime.cask"
-  requires "sublime-text.cask"
-end
---
+++ osx-base.rb
@@ -0,0 +1,8 @@
+# osx-base.rb
+
+dep 'osx-core-setup' do
+  requires 'osx-core-all'
+  requires 'osx-dev-ruby'
+  requires 'osx-dev-scm'
+  requires 'osx-settings'
+end
