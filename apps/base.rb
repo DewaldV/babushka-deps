1aa15196ad3ec757f36d7cd063b020948d6aa0c8
DewaldV <dewald.viljoen@gmail.com>
2014-09-25 11:51:45 +0200

    Add core dependency set tasks.

--- apps/base.rb
+++ apps/base.rb
@@ -26,4 +26,26 @@ dep 'vim.bin'
 
 dep 'xquartz.cask'
 
-dep 'zsh.bin'
+dep 'zsh.bin' 
+
+dep 'osx-core-all' do
+  requires 'osx-core-apps'
+  requires 'osx-core-apps-extended'
+end
+
+dep 'osx-core-apps' do
+  requires 'homebrew-cask.lib'
+  requires 'dropbox.cask'
+  requires 'htop.bin'
+  requires 'iterm2.cask'
+  requires 'mvim.bin'
+  requires 'onepassword.cask'
+  requires 'vim.bin'
+  requires 'zsh.bin'
+end
+
+dep 'osx-core-apps-extended' do
+  requires 'rescuetime.cask'
+  requires 'spectacle.cask'
+  requires 'xquartz.cask'
+end
