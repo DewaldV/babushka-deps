c5be40b37b72558141b6d42d64323202d542e67b
DewaldV <dewald.viljoen@gmail.com>
2015-12-17 15:42:43 +0200

    Remove shell call for directory existence, use ruby method.

--- templates/osx_dockutil.rb
+++ templates/osx_dockutil.rb
@@ -1,11 +1,9 @@
-# osx-dockutil.rb
-
 meta :dockicon do
   accepts_value_for :label
   accepts_value_for :path
 
   template do
-    met? { !shell?("[ -d #{path} ]") || shell?("dockutil --find #{label}") }
+    met? { !path.dir? || shell?("dockutil --find #{label}") }
     meet { shell("dockutil --add #{path} --label #{label}") }
   end
 end
