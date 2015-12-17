7846c73fb19cb70d9204c72d0f984d8a5119fa7e
DewaldV <dewald.viljoen@gmail.com>
2015-12-17 15:23:44 +0200

    Fixed issue with scutil template failing if a pref isn't set at all.

--- templates/osx_scutil.rb
+++ templates/osx_scutil.rb
@@ -1,11 +1,9 @@
-# osx-scutil.rb
-
 meta :scutil do
   accepts_value_for :perf
   accepts_value_for :value
 
   template do
-    met? { shell("scutil --get #{perf}") == value }
+    met? { shell?("scutil --get #{perf}") && shell("scutil --get #{perf}") == value }
     meet { sudo("scutil --set #{perf} #{value}") }
   end
 end
