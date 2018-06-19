909abe86e6cfe9810e2bf21e2c13f78a59ccf8a6
DewaldV <dewald.viljoen@uswitch.com>
2018-06-19 16:27:33 +0100

    Go and ruby ides

--- apps/dev/go.rb
+++ apps/dev/go.rb
@@ -1,9 +1,17 @@
 # go.rb
 
+dep 'dep.bin'
 dep 'go.bin'
+dep 'goland.cask'
 dep 'upx.bin'
 
 dep 'osx-dev-go' do
   requires 'go.bin'
   requires 'upx.bin'
+  requires 'dep.bin'
+end
+
+dep 'osx-dev-go-ide' do
+  requires 'osx-dev-go'
+  requires 'goland.cask'
 end
--- apps/dev/ruby.rb
+++ apps/dev/ruby.rb
@@ -6,7 +6,14 @@ dep 'rbenv.bin'
 
 dep 'ruby-build.bin'
 
+dep 'rubymine.cask'
+
 dep 'osx-dev-ruby' do
   requires 'ruby-build.bin'
   requires 'rbenv.bin'
+end
+
+dep 'osx-dev-ruby-ide' do
+  requires 'osx-dev-ruby'
+  requires 'rubymine.cask'
 end
