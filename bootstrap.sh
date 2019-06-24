22d02e5770cd812f87e0cb439d5c31a4e1354640
DewaldV <dewald.viljoen@uswitch.com>
2019-06-24 14:08:43 +0100

    OSX fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@ sudo mkdir -p /usr/local/bin
 sudo chown -R `whoami` /usr/local/bin
 
 xcode-select --install
+#OSX Mojave fix
+sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+
 type babushka || sh -c "`curl https://babushka.me/up`"
 
 ln -s $(pwd) ~/.babushka/deps
